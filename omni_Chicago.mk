02d69f0cf93577ccc6367f9c06d4e1e8a63bb708
Monster289 <3180454103@qq.com>
2023-11-12 15:53:09 +0800

    add config
    add Chicago config

---
+++ omni_Chicago.mk
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 PitchBlack Recovery Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := Chicago
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+DEVICE_PATH := device/tcl/Chicago
+
+# Inherit some common PitchBlack stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
+    $(LOCAL_PATH)/prebuilt/dtb:dtb.img
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := picasso
+PRODUCT_NAME := omni_picasso
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi K30 5G
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_RELEASE_NAME := Redmi K30 5G
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="Redmi/picasso/picasso:10/QKQ1.191117.002/20.1.13:user/release-keys" \
+    PRIVATE_BUILD_DESC="picasso-user 10 QKQ1.191117.002 20.1.13 release-keys"
+
+# Stock flashable zips
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.allow.mock.location=1
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31 \
+    ro.build.date=Tue May 3 20:05:48 KST 2016 \
+    ro.build.date.utc=1462273548
